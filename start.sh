b45f6d89b3e5ab8a7a062174c2405a9948cc13aa
TRCrazy4u <98374670+TRCrazy4u@users.noreply.github.com>
2022-01-28 11:09:48 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/TRCrazy4u/AzumaLimBot.git /AzumaLimBot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AzumaLimBot
 fi
-cd /EvaMaria
+cd /AzumaLimBot
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
