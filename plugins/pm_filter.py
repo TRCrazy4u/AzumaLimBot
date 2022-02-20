3ac135ad5031672b9ac42bbf5c0571b0129d8d9a
Hodaka <90975856+h0daka@users.noreply.github.com>
2022-02-20 16:41:21 +0530

    Done

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -14,7 +14,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQ
 from pyrogram import Client, filters
 from pyrogram.errors import FloodWait, UserIsBlocked, MessageNotModified, PeerIdInvalid
 from utils import get_size, is_subscribed, get_poster, search_gagala, temp, get_settings, save_group_settings
-from database.users_chats_db import db
+from database.users_chats_db import db, is_filter_on, filter_on, filter_off
 from database.ia_filterdb import Media, get_file_details, get_search_results
 from database.filters_mdb import (
     del_all,
@@ -35,6 +35,8 @@ async def give_filter(client, message):
     k = await manual_filters(client, message)
     if k == False:
         await auto_filter(client, message)
+    if not await is_filter_on(message.chat.id):
+        return
 
 
 @Client.on_callback_query(filters.regex(r"^next"))
@@ -827,3 +829,26 @@ async def manual_filters(client, message, text=False):
                 break
     else:
         return False
+
+@Client.on_message(filters.command("af") & ~filters.private)
+async def filter_enable_disable(_, message):
+    if len(message.command) != 2:
+        await message.reply_text(
+            "Usage: /af [on | off]"
+        )
+        return
+    status = message.text.split(None, 1)[1].strip()
+    status = status.lower()
+    chat_id = message.chat.id
+    if status == "on":
+        filter_on(chat_id)
+        await message.reply_text(
+            "Enabled Auto filter System. I will Delete Messages Containing Inappropriate Content."
+        )
+    elif status == "off":
+        filter_off(chat_id)
+        await message.reply_text("Disabled Auto filter System.")
+    else:
+        await message.reply_text(
+            "`Use /af on/off`"
+        )
